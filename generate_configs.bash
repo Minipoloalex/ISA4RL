da3330ef065d590a327990a61e3d91aa078c1e19
Félix Martins <felix.marcial@hotmail.com>
2026-05-06 14:57:30 +0100

    Removed outdated files and script

--- generate_configs.bash
+++
@@ -1,6 +0,0 @@
-cd src/main/
-uv run --env-file .env ../agents_highway/build_configs.py > ../../config/logs_he.txt
-
-cd ../../
-cd src/agents_metadrive/
-uv run build_configs.py > ../../config/logs_md.txt
