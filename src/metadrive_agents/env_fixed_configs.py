a219f7e0ee29c4217de41637481db2ae16dcd8f8
Félix Martins <felix.marcial@hotmail.com>
2026-05-19 23:04:50 +0100

    Made metadrive configs also test/evaluate for 50 episodes instead of 10

--- src/metadrive_agents/env_fixed_configs.py
+++ src/metadrive_agents/env_fixed_configs.py
@@ -7,7 +7,7 @@ METADRIVE_FIXED_CONFIGS = {
     "train_timesteps": int(1e6),
     "eval_freq": int(1e4),
     "n_eval_episodes": 5,
-    "n_test_episodes": 10,
+    "n_test_episodes": 50,
     "config": { # when instantiating the config, need to be careful with recursive dicts
         "random_traffic": False,
         "num_scenarios": int(1e12),
